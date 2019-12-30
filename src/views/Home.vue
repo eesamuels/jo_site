21d551da9fb266aa82cfce5d0591bb4c34c56f34
eesamuels <emmasamuelspdx@gmail.com>
2019-12-29 18:18:32 -0800

    Stable Grid - Hard Coded

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,21 +1,233 @@
 <template>
-  <div id="app">
-    <div>
-      <input type="text"
-        v-model="newReptile"
-        @keyup.enter="addReptile">
-      <button  @click="addReptile">
-        Add Reptile
-      </button>
+  <div id="app" style="margin-top:0px;">
+    
+       <div  class="mx-auto" style="width:85%" >
+        <div d-inline-block id="nav">
+      <span left> <v-img height="100" contain :src="`https://firebasestorage.googleapis.com/v0/b/to-do-ac295.appspot.com/o/signature.png?alt=media&token=491b826d-2db0-4bd7-b1ca-d910d1a10e5c`" /> </span> 
+      <span right> PORTFOLIO | ABOUT | CONTACT </span>
+      <br> &nbsp; <br>
     </div>
-    <ul class="reptileList">
-      <li v-for="reptile in reptiles" :key="reptile">
-        {{ reptile.name }} -
-        <button @click="deleteReptile(reptile)">
-          Remove
-        </button>
-      </li>
-    </ul>
+        <v-card>
+          <v-container fluid>
+            <!--- Row Start --->
+            <v-row>
+              <!--- Picture 1 --->
+              <v-col
+                class="d-flex child-flex"
+                cols="4"
+              >
+                <v-card :elevation="4" flat tile class="d-flex">
+                  <v-img
+                    :src="`https://firebasestorage.googleapis.com/v0/b/to-do-ac295.appspot.com/o/StockPaintings%2F1.jpeg?alt=media&token=dd53ea23-46c2-4871-b6d0-a31f3ceb4836`"
+                    aspect-ratio="1"
+                    class="grey lighten-2"
+                  >
+                    <template v-slot:placeholder>
+                      <v-row
+                        class="fill-height ma-0"
+                        align="center"
+                        justify="center"
+                      >
+                        <v-progress-circular indeterminate color="grey lighten-5"></v-progress-circular>
+                      </v-row>
+                    </template>
+                  </v-img>
+                </v-card >
+              </v-col>
+              <!--- Picture 2 --->
+              <v-col
+                class="d-flex child-flex"
+                cols="4"
+              >
+                <v-card :elevation="4"  flat tile class="d-flex">
+                  <v-img
+                    :src="`https://firebasestorage.googleapis.com/v0/b/to-do-ac295.appspot.com/o/StockPaintings%2F2.jpeg?alt=media&token=feca5790-eccd-48f8-ba78-07eff026f13e`"
+                    aspect-ratio="1"
+                    class="grey lighten-2"
+                  >
+                    <template v-slot:placeholder>
+                      <v-row
+                        class="fill-height ma-0"
+                        align="center"
+                        justify="center"
+                      >
+                        <v-progress-circular indeterminate color="grey lighten-5"></v-progress-circular>
+                      </v-row>
+                    </template>
+                  </v-img>
+                </v-card >
+              </v-col>
+              <!--- Picture 3 --->
+              <v-col
+                class="d-flex child-flex"
+                cols="4"
+              >
+                <v-card :elevation="4"  flat tile class="d-flex">
+                  <v-img
+                    :src="`https://firebasestorage.googleapis.com/v0/b/to-do-ac295.appspot.com/o/StockPaintings%2F3.jpeg?alt=media&token=9ad5e245-cffa-43cf-96e9-f6054933925f`"
+                    aspect-ratio="1"
+                    class="grey lighten-2"
+                  >
+                    <template v-slot:placeholder>
+                      <v-row
+                        class="fill-height ma-0"
+                        align="center"
+                        justify="center"
+                      >
+                        <v-progress-circular indeterminate color="grey lighten-5"></v-progress-circular>
+                      </v-row>
+                    </template>
+                  </v-img>
+                </v-card >
+              </v-col>
+            </v-row>
+             <!--- Row Start --->
+            <v-row>
+              <!--- Picture 1 --->
+              <v-col
+                class="d-flex child-flex"
+                cols="4"
+              >
+                <v-card :elevation="4"  flat tile class="d-flex">
+                  <v-img
+                    :src="`https://firebasestorage.googleapis.com/v0/b/to-do-ac295.appspot.com/o/StockPaintings%2F4.jpeg?alt=media&token=89329ebb-97b9-47a4-ab3f-6ba16b6f6608`"
+                    aspect-ratio="1"
+                    class="grey lighten-2"
+                  >
+                    <template v-slot:placeholder>
+                      <v-row
+                        class="fill-height ma-0"
+                        align="center"
+                        justify="center"
+                      >
+                        <v-progress-circular indeterminate color="grey lighten-5"></v-progress-circular>
+                      </v-row>
+                    </template>
+                  </v-img>
+                </v-card >
+              </v-col>
+              <!--- Picture 2 --->
+              <v-col
+                class="d-flex child-flex"
+                cols="4"
+              >
+                <v-card :elevation="4"  flat tile class="d-flex">
+                  <v-img
+                    :src="`https://firebasestorage.googleapis.com/v0/b/to-do-ac295.appspot.com/o/StockPaintings%2F5.jpeg?alt=media&token=8896d0f9-a937-4ac8-a617-b575831c3f35`"
+                    aspect-ratio="1"
+                    class="grey lighten-2"
+                  >
+                    <template v-slot:placeholder>
+                      <v-row
+                        class="fill-height ma-0"
+                        align="center"
+                        justify="center"
+                      >
+                        <v-progress-circular indeterminate color="grey lighten-5"></v-progress-circular>
+                      </v-row>
+                    </template>
+                  </v-img>
+                </v-card >
+              </v-col>
+              <!--- Picture 3 --->
+              <v-col
+                class="d-flex child-flex"
+                cols="4"
+              >
+                <v-card :elevation="4"  flat tile class="d-flex">
+                  <v-img
+                    :src="`https://firebasestorage.googleapis.com/v0/b/to-do-ac295.appspot.com/o/StockPaintings%2F6.jpeg?alt=media&token=f2c2a269-bcb9-4462-8cf1-151833458f83`"
+                    aspect-ratio="1"
+                    class="grey lighten-2"
+                  >
+                    <template v-slot:placeholder>
+                      <v-row
+                        class="fill-height ma-0"
+                        align="center"
+                        justify="center"
+                      >
+                        <v-progress-circular indeterminate color="grey lighten-5"></v-progress-circular>
+                      </v-row>
+                    </template>
+                  </v-img>
+                </v-card >
+              </v-col>
+            </v-row>
+             <!--- Row Start --->
+            <v-row>
+              <!--- Picture 1 --->
+              <v-col
+                class="d-flex child-flex"
+                cols="4"
+              >
+                <v-card :elevation="4"  flat tile class="d-flex">
+                  <v-img
+                    :src="`https://firebasestorage.googleapis.com/v0/b/to-do-ac295.appspot.com/o/StockPaintings%2F8.jpeg?alt=media&token=f3d71510-bace-4e23-8e81-f58587b59229`"
+                    aspect-ratio="1"
+                    class="grey lighten-2"
+                  >
+                    <template v-slot:placeholder>
+                      <v-row
+                        class="fill-height ma-0"
+                        align="center"
+                        justify="center"
+                      >
+                        <v-progress-circular indeterminate color="grey lighten-5"></v-progress-circular>
+                      </v-row>
+                    </template>
+                  </v-img>
+                </v-card >
+              </v-col>
+              <!--- Picture 2 --->
+              <v-col
+                class="d-flex child-flex"
+                cols="4"
+              >
+                <v-card :elevation="4"  flat tile class="d-flex">
+                  <v-img
+                    :src="`https://firebasestorage.googleapis.com/v0/b/to-do-ac295.appspot.com/o/StockPaintings%2F9.jpeg?alt=media&token=b59b309e-2391-4272-85b3-2a7c79a58f9e`"
+                    aspect-ratio="1"
+                    class="grey lighten-2"
+                  >
+                    <template v-slot:placeholder>
+                      <v-row
+                        class="fill-height ma-0"
+                        align="center"
+                        justify="center"
+                      >
+                        <v-progress-circular indeterminate color="grey lighten-5"></v-progress-circular>
+                      </v-row>
+                    </template>
+                  </v-img>
+                </v-card >
+              </v-col>
+              <!--- Picture 3 --->
+              <v-col
+                class="d-flex child-flex"
+                cols="4"
+              >
+                <v-card :elevation="4"  flat tile class="d-flex">
+                  <v-img
+                    :src="`https://firebasestorage.googleapis.com/v0/b/to-do-ac295.appspot.com/o/StockPaintings%2F1.jpeg?alt=media&token=dd53ea23-46c2-4871-b6d0-a31f3ceb4836`"
+                    aspect-ratio="1"
+                    class="grey lighten-2"
+                  >
+                    <template v-slot:placeholder>
+                      <v-row
+                        class="fill-height ma-0"
+                        align="center"
+                        justify="center"
+                      >
+                        <v-progress-circular indeterminate color="grey lighten-5"></v-progress-circular>
+                      </v-row>
+                    </template>
+                  </v-img>
+                </v-card >
+              </v-col>
+            </v-row>
+          </v-container>
+        </v-card >
+       </div>
   </div>
 </template>
 
@@ -26,13 +238,15 @@
     name: 'app',
     data() {
       return {
-        reptiles: [],
-        newReptile: ''
+        title: [],
+        artist_statement: [],
+        meta: [],
+        img: [],
       }
     },
     firestore() {
       return {
-        reptiles: db.collection('reptiles'),
+        paintings: db.collection('paintings'),
       }
     },
     methods: {
@@ -47,8 +261,8 @@
       },
       deleteReptile: function(reptile) {
         this.$firestore.reptiles.doc(reptile['.key']).delete();
-      }
-    }
+      },
+    },
   }
 </script>
 
@@ -59,7 +273,7 @@
     -moz-osx-font-smoothing: grayscale;
     text-align: center;
     color: #2c3e50;
-    margin-top: 60px;
+    padding-bottom: 50px;
   }
   .reptileList {
     list-style: none;
